fe99ccc961a4b7909a16189628d9e1cbcfca70ca
Fabiano C. de Oliveira <fabiano.oliveira@zup.com.br>
2024-10-06 15:02:29 -0300

    refactoring names

--- src/main/java/br/com/facio/lab/demo/HelloController.java
+++ src/main/java/br/com/facio/lab/demo/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package br.com.facio.lab.demo;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
--- src/main/java/br/com/facio/lab/demo/SimpleLabApplication.java
+++ src/main/java/br/com/facio/lab/demo/SimpleLabApplication.java
@@ -1,11 +1,11 @@
-package com.example.demo;
+package br.com.facio.lab.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SimpleLabApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SimpleLabApplication.class, args);
     }
 }
